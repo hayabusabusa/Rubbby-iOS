4c65c692b4e0eccc86c41232f19e34eda8387645
hayabusabusa <hayabusabusaSY@gmail.com>
2020-02-13 17:23:02 +0900

    Add translation entity and enum

---
+++ Rubbby/Domain/Entity/TranslationEntity.swift
@@ -0,0 +1,19 @@
+//
+//  TranslationEntity.swift
+//  Rubbby
+//
+//  Created by Yamada Shunya on 2020/02/13.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import Foundation
+
+struct TranslationEntity: Decodable {
+    let converted: String
+    let outputType: TranslationType
+    
+    private enum CodingKeys: String, CodingKey {
+        case converted
+        case outputType = "output_type"
+    }
+}
---
+++ Rubbby/Domain/Entity/TranslationType.swift
@@ -0,0 +1,14 @@
+//
+//  TranslationType.swift
+//  Rubbby
+//
+//  Created by Yamada Shunya on 2020/02/13.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import Foundation
+
+enum TranslationType: String, Decodable {
+    case hiragana
+    case katakana
+}
