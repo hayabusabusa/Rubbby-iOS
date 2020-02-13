fe42e3be76683606a56c7c5d22286b22555943d3
hayabusabusa <31949692+hayabusabusa@users.noreply.github.com>
2020-02-14 01:53:37 +0900

    Fix moya target and add service class

---
+++ Rubbby/Domain/Network/HiraganaTranslationAPIService.swift
@@ -0,0 +1,40 @@
+//
+//  HiraganaTranslationAPIService.swift
+//  Rubbby
+//
+//  Created by 山田隼也 on 2020/02/14.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import Foundation
+import Moya
+import RxMoya
+import RxSwift
+
+protocol HiraganaTranslationAPI {
+    func request<T: HiraganaTranslationAPITargetType>(_ request: T) -> Single<T.Response>
+}
+
+final class HiraganaTranslationAPIService: HiraganaTranslationAPI {
+    
+    // MARK: Singletone
+    
+    static let shared: HiraganaTranslationAPIService = .init()
+    
+    // MARK: Properties
+    
+    private let provider: MoyaProvider<MultiTarget> = .init()
+    
+    // MARK: Initializer
+    
+    private init() {}
+    
+    // MARK: Request
+    
+    func request<T: HiraganaTranslationAPITargetType>(_ request: T) -> Single<T.Response> {
+        let target = MultiTarget(request)
+        return provider.rx.request(target)
+            .filterSuccessfulStatusCodes()
+            .map(T.Response.self)
+    }
+}
---
+++ Rubbby/Domain/Network/HiraganaTranslationAPITargetType.swift
@@ -0,0 +1,27 @@
+//
+//  HiraganaTranslationAPITargetType.swift
+//  Rubbby
+//
+//  Created by 山田隼也 on 2020/02/14.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+protocol HiraganaTranslationAPITargetType: TargetType {
+    associatedtype Response: Decodable
+    
+    var parameters: [String: Any] { get }
+    var parameterEncoding: Moya.URLEncoding { get }
+}
+
+extension HiraganaTranslationAPITargetType {
+    var baseURL: URL {
+        return URL(string: "https://labs.goo.ne.jp/api")!
+    }
+    
+    var headers: [String : String]? {
+        return nil
+    }
+}
