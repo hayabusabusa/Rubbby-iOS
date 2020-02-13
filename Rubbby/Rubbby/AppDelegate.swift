b9052f27d173198dc58700cb64311169c3dcfa9b
hayabusabusa <hayabusabusaSY@gmail.com>
2020-02-13 13:16:57 +0900

    Add Xcode projects

---
+++ Rubbby/Rubbby/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Rubbby
+//
+//  Created by Yamada Shunya on 2020/02/13.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
