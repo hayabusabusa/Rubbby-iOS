656d25b7ec08670c85d12e558fd7163626056085
hayabusabusa <hayabusabusaSY@gmail.com>
2020-02-14 13:08:45 +0900

    Add prototype view model

---
+++ Rubbby/ViewModel/Prototype/PrototypeViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  PrototypeViewModel.swift
+//  Rubbby
+//
+//  Created by Yamada Shunya on 2020/02/14.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+
+final class PrototypeViewModel {
+    
+    // MARK: Dependency
+    
+    // MARK: Propreties
+    
+    private let disposeBag = DisposeBag()
+    
+    // MARK: Initializer
+}
+
+extension PrototypeViewModel {
+    
+    // MARK: I/O
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    // MARK: Transform I/O
+    
+    func transform(input: PrototypeViewModel.Input) -> PrototypeViewModel.Output {
+        return Output()
+    }
+}
