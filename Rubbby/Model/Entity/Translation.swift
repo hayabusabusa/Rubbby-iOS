5b55e703d4f8eaba4e544236e1f2f96ec5901e5b
hayabusabusa <hayabusabusaSY@gmail.com>
2020-02-14 11:22:27 +0900

    Add translation entity translator

---
+++ Rubbby/Model/Entity/Translation.swift
@@ -0,0 +1,14 @@
+//
+//  Translation.swift
+//  Rubbby
+//
+//  Created by Yamada Shunya on 2020/02/14.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import Foundation
+
+struct Translation {
+    let converted: String
+    let outputType: TranslationType
+}
---
+++ Rubbby/Model/Translator/TranslationEntityTranslator.swift
@@ -0,0 +1,18 @@
+//
+//  TranslationEntityTranslator.swift
+//  Rubbby
+//
+//  Created by Yamada Shunya on 2020/02/14.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import Foundation
+
+struct TranslationEntityTranslator: Translator {
+    typealias Input = TranslationEntity
+    typealias Output = Translation
+    
+    func translate(_ input: TranslationEntity) throws -> Translation {
+        return Translation(converted: input.converted, outputType: input.outputType)
+    }
+}
