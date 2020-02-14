58ab30405a0755f38b8772ac13704900d90b7241
hayabusabusa <hayabusabusaSY@gmail.com>
2020-02-14 12:59:04 +0900

    Add prototype vc

---
+++ Rubbby/View/Prototype/PrototypeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PrototypeViewController.swift
+//  Rubbby
+//
+//  Created by Yamada Shunya on 2020/02/14.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import UIKit
+
+final class PrototypeViewController: UIViewController {
+    
+    // MARK: IBOutlet
+    
+    // MARK: Properties
+    
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
