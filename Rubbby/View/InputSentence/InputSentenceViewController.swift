66f444f2cca4d0ff1fa45dad385b2c5c7fec8038
hayabusabusa <hayabusabusaSY@gmail.com>
2020-02-17 16:52:46 +0900

    Add input sentence view controller and disposable view controller

---
+++ Rubbby/View/InputSentence/InputSentenceViewController.swift
@@ -0,0 +1,22 @@
+//
+//  InputSentenceViewController.swift
+//  Rubbby
+//
+//  Created by Yamada Shunya on 2020/02/17.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import UIKit
+
+final class InputSentenceViewController: DisposableViewController {
+    
+    // MARK: IBOutlet
+    
+    // MARK: Properties
+    
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
