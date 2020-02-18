f07406ea294af8b392cc3ee975ca214178829c95
hayabusabusa <31949692+hayabusabusa@users.noreply.github.com>
2020-02-19 01:26:23 +0900

    Add database repository

---
+++ Rubbby/Model/Repository/DatabaseRepository.swift
@@ -0,0 +1,42 @@
+//
+//  DatabaseRepository.swift
+//  Rubbby
+//
+//  Created by 山田隼也 on 2020/02/19.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+// MARK: - Interface
+
+protocol DatabaseRepository {
+    func saveHistory(_ history: HistoryEntity) -> Completable
+    func getHistories() -> Single<[HistoryEntity]>
+}
+
+// MARK: - Implementation
+
+struct DatabaseRepositoryImpl: DatabaseRepository {
+    
+    // MARK: Dependency
+    
+    private let realmManager: RealmManagerProtocol
+    
+    // MARK: Initialzer
+    
+    init(realmManager: RealmManagerProtocol = RealmManager()) {
+        self.realmManager = realmManager
+    }
+    
+    // MARK: Realm
+    
+    func saveHistory(_ history: HistoryEntity) -> Completable {
+        return realmManager.save(history)
+    }
+    
+    func getHistories() -> Single<[HistoryEntity]> {
+        return realmManager.get(HistoryEntity.self)
+    }
+}
