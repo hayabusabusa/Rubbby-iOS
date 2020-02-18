86e4ac6f03c83b6be4ab63e6b22773c84341a660
hayabusabusa <31949692+hayabusabusa@users.noreply.github.com>
2020-02-18 23:27:56 +0900

    Add history entity

---
+++ Rubbby/Model/Entity/History.swift
@@ -0,0 +1,14 @@
+//
+//  History.swift
+//  Rubbby
+//
+//  Created by 山田隼也 on 2020/02/18.
+//  Copyright © 2020 Shunya Yamada. All rights reserved.
+//
+
+import Foundation
+
+struct History {
+    let original: String
+    let converted: String
+}
